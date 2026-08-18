26b10e14ade06d13d1231863bcb03a2a025c1034
Cecil Lwala <lwalacecil13@gmail.com>
2026-08-18 16:16:48 +0300

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxArea(int[] heights) {
+
+        int left = 0;
+        int right = heights.length - 1;
+        int maxArea = 0;
+
+        while(left < right){
+            int height = Math.min(heights[left], heights[right]);
+            int width = right - left;
+            int product = height * width;
+
+            if(product > maxArea){
+                maxArea = product;
+            }
+
+            if(heights[left] > heights[right]){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+
+        return maxArea;
+        
+    }
+}
