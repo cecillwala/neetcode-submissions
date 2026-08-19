cefa1d50a2a5eaa1bbe92e7c6247e4583619bec7
Cecil Lwala <lwalacecil13@gmail.com>
2026-08-19 16:00:37 +0300

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int totalVolume = 0;
+
+        int rightMax = 0;
+        int leftMax = 0;
+
+        while (left < right){
+            if(height[left] < height[right]){
+                leftMax = Math.max(leftMax, height[left]);
+                totalVolume += leftMax - height[left];
+                left++;
+            }
+            else{
+                rightMax = Math.max(rightMax, height[right]);
+                totalVolume += rightMax - height[right];
+                right--;
+            }
+        }
+
+        return totalVolume;
+    }
+}
